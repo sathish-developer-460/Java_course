f6c180eb3433850c78cc24a6930b3051608a5046
sathish-developer-460 <ch.sathish460@gmail.com>
2026-08-06 14:31:35 +0530

    Class is updated with Example

--- 10.OOP's/1.Class&Object/i.ClassExample/ClassExample.java
+++ 10.OOP's/1.Class&Object/i.ClassExample/ClassExample.java
@@ -16,26 +16,53 @@
  * • accelerate() is a method that defines behavior.
  */
 
+//Example1:-
+
 class Car {
     String brand = "Audi"; // attribute1
     int speed = 120; // attribute2
 
     // method
     void accelerate() {
-        System.out.println(brand + " is accelerating at " + speed + " km/h. ");
-        // Toyota is accelerating at 160 km/h.
+        System.out.println(brand + " is accelerating at " + speed + " km/hr ");
+        // Audi is accelerating at 120 km/hr
     }
 }
 
 class ClassExample {
     public static void main(String[] args) {
         // creating a new object
-        Car car1 = new Car();
-        car1.brand = "Toyota"; // Setting attributes
-        car1.speed = 160;
-        car1.accelerate(); // Calling method
-        // System.out.println(car1.brand + " is accelerating at " + car1.speed + " km/h.
-        // ");
-        // Toyota is accelerating at 160 km/h.
+        Car car1 = new Car(); // creating an object of the class Car
+        car1.accelerate(); // calling the method of the object
+        System.out.println(car1.brand + " is accelerating at " + car1.speed + " km/hr ");
+        // Audi is accelerating at 120 km/hr
+
     }
 }
+
+// Example2:-
+
+// class Car {
+// String brand = "Audi"; // attribute1
+// int speed = 120; // attribute2
+
+// // method
+// void accelerate() {
+// System.out.println(brand + " is accelerating at " + speed + " km/h. ");
+// // Toyota is accelerating at 160 km/h.
+// }
+// }
+
+// class ClassExample {
+// public static void main(String[] args) {
+// // creating a new object
+// Car car1 = new Car();
+// car1.brand = "Toyota"; // Setting attributes
+// car1.speed = 160;
+// car1.accelerate(); // Calling method
+// // System.out.println(car1.brand + " is accelerating at " + car1.speed + "
+// km/h.
+// // ");
+// // Toyota is accelerating at 160 km/h.
+// }
+// }
